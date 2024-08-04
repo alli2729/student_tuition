b44b01bb83a386b732d5860609083d3cd3744a69
Ali Karimi <a.karimi2729@gmail.com>
2024-08-05 02:58:57 +0330

    fix

--- main.dart
+++ main.dart
@@ -16,15 +16,13 @@ void main(List<String> args) {
   taaviUniversity.addSemester(id: 1, title: '2024 summer');
 
   // add new cource to (2024 summer) semester
-  taaviUniversity.Semesters.first.addCourse(
+  taaviUniversity.Semesters.first.addSpecializedCourse(
     title: 'Math 1',
     id: 1001,
-    unitCount: 3,
   );
 
-  taaviUniversity.Semesters.first.addCourse(
+  taaviUniversity.Semesters.first.addGeneralCourse(
     title: 'Ping Pong',
     id: 1002,
-    unitCount: 2,
   );
 }
