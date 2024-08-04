b2ebfe8276fa297471dcb8f9642cc4f16d52af31
Ali Karimi <a.karimi2729@gmail.com>
2024-08-05 02:57:22 +0330

    change to abstract

--- course.dart
+++ course.dart
@@ -1,19 +1,18 @@
-class Course {
+abstract class Course {
   // Variabl e
   String title;
   int id;
-  int unitCount;
+  // int unitCount;
 
   // Constractor
   Course({
     required this.title,
     required this.id,
-    required this.unitCount,
   });
   // Methods
 
   @override
   String toString() {
-    return 'title: $title , id: $id, unitCount: $unitCount';
+    return 'title: $title , id: $id';
   }
 }
