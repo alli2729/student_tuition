c894da63d1daee52d63d7fdbd9cd0b995cb6d624
Ali Karimi <a.karimi2729@gmail.com>
2024-08-07 18:00:13 +0330

    changed structure of programm

--- main.dart
+++ main.dart
@@ -1,4 +1,9 @@
+import 'course.dart';
+import 'general_course.dart';
 import 'location.dart';
+import 'semester.dart';
+import 'specialized_course.dart';
+import 'student.dart';
 import 'university.dart';
 
 void main(List<String> args) {
@@ -13,20 +18,25 @@ void main(List<String> args) {
   );
 
   // add semester
-  taaviUniversity.addSemester(id: 1, title: '2024 summer');
+  Semester summer2024 = Semester(id: 1, title: '2024 summer');
+  taaviUniversity.addSemester(semester: summer2024);
 
   // add new cource to (2024 summer) semester
-  taaviUniversity.Semesters.first.addSpecializedCourse(
-    title: 'Math 1',
-    id: 1001,
-  );
+  Course math1 = SpecializedCourse(title: 'Math1', id: 1001);
+  Course pingPong = GeneralCourse(title: 'Ping Pong', id: 1002);
 
-  taaviUniversity.Semesters.first.addGeneralCourse(
-    title: 'Ping Pong',
-    id: 1002,
-  );
+  taaviUniversity.Semesters.first.addCourse(course: math1);
+  taaviUniversity.Semesters.first.addCourse(course: pingPong);
 
   // add students
-  taaviUniversity.addStudent(name: 'ali karimi', id: 1, location: shiraz);
-  taaviUniversity.addStudent(name: 'sina rezaei', id: 2, location: shiraz);
+  Student s1 = Student(name: 'ali karimi', id: 1, location: shiraz);
+  Student s2 = Student(name: 'sina rezaei', id: 2, location: shiraz);
+
+  taaviUniversity.addStudent(student: s1);
+  taaviUniversity.addStudent(student: s2);
+
+  // add students to Course
+  taaviUniversity.Semesters.first.addStudentToCourse(student: s1);
+
+  print(summer2024);
 }
