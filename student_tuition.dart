60b508a2dc8d38a79f9157ec8a3b2f66ffe79534
Ali Karimi <a.karimi2729@gmail.com>
2024-08-11 21:28:31 +0330

    init

--- student_tuition.dart
+++ student_tuition.dart
@@ -1,14 +1,56 @@
+import 'semester.dart';
+import 'student.dart';
+import 'university.dart';
+
 class StudentTuition {
   // Variable
+  Student student;
+  Semester semester;
+  University uni;
 
   // Constractor
+  StudentTuition({
+    required this.student,
+    required this.semester,
+    required this.uni,
+  });
 
   // Methods
-  // TODO :(calculate semester cost)
+  int calculate({required int studentId, required int semesterId}) {
+    // create semester by ID
+    final Semester? semester = uni.getSemesterById(semesterId: semesterId);
+    // create student by ID
+    final Student? student = uni.getStudentById(studentId: studentId);
+
+    if (student != null && semester != null) {
+      // if student.location == uni.location
+      int costPerUnit = 75;
+      // else
+      if (student.location != uni.location) costPerUnit = 125;
+
+      int totalUnits = 0;
+      for (var course in semester.courses) {
+        // if student has course
+        if (course.studentInCourse.contains(student)) {
+          totalUnits += course.unitCount;
+        }
+      }
+
+      print('totalUnits :$totalUnits');
+      print('costPerUnit :$costPerUnit');
+
+      // calculation
+      int totalcost = costPerUnit * totalUnits;
+
+      if (totalUnits < 7) totalcost += 100;
+
+      return totalcost;
+    }
+    return -1;
+  }
 
   @override
   String toString() {
-    // TODO: implement toString
-    return '';
+    return 'student: $student, semester: $semester, uni: $uni';
   }
 }
